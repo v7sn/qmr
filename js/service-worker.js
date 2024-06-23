7e01903ab7dc67e20626d3a2285c5797e047597a
Hassan Abdulreda <108088689+v7sn@users.noreply.github.com>
2024-06-23 22:24:26 +0300

    Create service-worker.js

---
+++ js/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        'logo-192x192.png',
+        'logo-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+}); 
